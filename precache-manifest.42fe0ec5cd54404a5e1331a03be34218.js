ed105fcb1b2a2bf0a80168d9b934ae1b77943d39
BayLadyCoder <rachada_32@hotmail.com>
2019-08-11 01:41:00 -0400

    Updates

---
+++ precache-manifest.42fe0ec5cd54404a5e1331a03be34218.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9188aebe711c458687c3e65c443a798",
+    "url": "/color-box-maker/index.html"
+  },
+  {
+    "revision": "bcb922950efe569b134c",
+    "url": "/color-box-maker/static/css/main.215b786a.chunk.css"
+  },
+  {
+    "revision": "8d147c17fc28f99b2932",
+    "url": "/color-box-maker/static/js/2.364196d1.chunk.js"
+  },
+  {
+    "revision": "bcb922950efe569b134c",
+    "url": "/color-box-maker/static/js/main.9a216ac3.chunk.js"
+  },
+  {
+    "revision": "e6f1f25a00732e8d58d6",
+    "url": "/color-box-maker/static/js/runtime~main.c1fb49e7.js"
+  }
+]);
